9a5c488ae66ed686c412e290aea1bab9526c18b8
inam95 <mfihassan99@gmail.com>
2022-01-06 07:38:03 +0530

    heroku initialization

--- public/js/app.js
+++ public/js/app.js
@@ -13,16 +13,14 @@ weatherForm.addEventListener('submit', (e) => {
   messageTwo.textContent = '';
   messageOne.textContent = 'Loading...';
 
-  fetch(`http://localhost:4002/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+const port = process.env.PORT || 4002;
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public');
@@ -101,6 +102,6 @@ app.get('*', (req, res, next) => {
   });
 });
 
-app.listen(4002, () => {
-  console.log('Server is up on port 4002');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
